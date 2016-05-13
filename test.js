a8d4e60cb6331a007a96c53ed575607348f468a0
Hamed Abdollahpour <ha.hamed@gmail.com>
2016-05-13 10:52:54 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const render 	  = require('mithril-node-render');
 const component = require('./componentname');
 
-const ctrl = component.controller(data);
+const model = {/* sample data */};
+
+const ctrl = component.controller(model);
 const view = component.view(ctrl);
 const innerHtml = render(view);
 console.log(innerHtml);
