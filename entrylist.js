102a52a00bd858b07d28c60860ba17bbb1e40b7c
Juliette TRAN <tran.juliette.a@gmail.com>
2017-04-20 21:40:53 +0800

    Fix errors with entries

--- entrylist.js
+++ entrylist.js
@@ -14,34 +14,60 @@ const entry       = require("./entry");
 
 // Return an array using the controller of the entry mithril module.
 const controller = (data, colors) => {
-    return !(data && data.pages) ? [ entry.controller({}, [], colors) ] : data.pages.map(page => {
 
+    /*
+    for(let i = 0; i < data.entries.length; ++i) {
+        console.log(data.entries[i].request.url);
+    }
+    */
+   
+    // Sort entries from older started datetime to more recent.
+    let orderEntries = !data.entries ? [] : data.entries.sort((a, b) => {
+        const dateA = new Date(a.startedDateTime);
+        const dateB = new Date(b.startedDateTime);
+
+        if (dateA > dateB) {
+            return 1;
+        }
+        else if (dateA < dateB) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    });
+
+    /*
+    for(let i = 0; i < orderEntries.length; ++i) {
+        console.log(orderEntries[i].request.url);
+    }
+    */
+   
+    let pages = [{
+        startedDateTime: (orderEntries[0] && orderEntries[0].startedDateTime ? orderEntries[0].startedDateTime : 0),
+        id: (orderEntries[0] && orderEntries[0].pageref ? orderEntries[0].pageref : ""),
+        title: (orderEntries[0] && orderEntries[0].request && orderEntries[0].request.url ? orderEntries[0].request.url : ""),
+        pageTimings: {
+            onLoad: (orderEntries[0] && orderEntries[0].startedDateTime ? Date.parse(orderEntries[orderEntries.length - 1].startedDateTime) - Date.parse(orderEntries[0].startedDateTime) : 0)
+        }
+    }];
+
+    if (data && data.pages) {
+        pages = data.pages;
+    }
+
+    return pages.map(page => {
         // Filter entries, keep the one related to the page.
-        const filterEntries = !data.entries ? [] : data.entries.filter((entry) => {
-            if (entry.pageref == page.id) {
-                return true;
-            }
-            return false;
-        });
-
-        // Sort entries from older started datetime to more recent.
-        const orderEntries = filterEntries.sort((a, b) => {
-            const dateA = new Date(a.startedDateTime);
-            const dateB = new Date(b.startedDateTime);
-
-            if (dateA > dateB) {
-                return 1;
-            }
-            else if (dateA < dateB) {
-                return -1;
-            }
-            else {
-                return 0;
-            }
-        });
-
+        if (data && data.pages) {
+            orderEntries = orderEntries.filter((entry) => {
+                if (entry.pageref == page.id) {
+                    return true;
+                }
+                return false;
+            });
+        }
         return entry.controller(page, orderEntries, colors);
-  });
+    });
 }
 
 // Return a entries list by using the view of the entry mithril module.
--- test.js
+++ test.js
@@ -4,7 +4,24 @@ const fs            = require('fs');
 const render        = require('mithril-node-render');
 const harviewer     = require('./harviewer');
 
-const harFile = JSON.parse(fs.readFileSync("har.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/errorhar.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/errorhar2.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har(1).json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har(2).json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har(3).json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har(4).json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har(5).json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/test-har-bing.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/test-har.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/report.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/crosswalk.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/crosswalk2.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/baidu.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/baidu2.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/baidu3.json", 'UTF-8'));
+// const harFile = JSON.parse(fs.readFileSync("./hars/har.json", 'UTF-8'));
+const harFile = JSON.parse(fs.readFileSync("./hars/zh-luckyvitamin.json", 'UTF-8'));
 
 const ctrl = harviewer.controller(harFile);
 const view = harviewer.view(ctrl);
